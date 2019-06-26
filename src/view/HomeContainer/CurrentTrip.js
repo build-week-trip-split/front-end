97f79e10dd87b084e4b7c9357d535771a545bb6a
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-06-26 16:14:04 -0400

    address warnings

--- src/view/HomeContainer/CurrentTrip.js
+++ src/view/HomeContainer/CurrentTrip.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Route, Link, withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const CurrentTrip = props => {
   console.log(props);
