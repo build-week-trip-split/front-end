4cb8923251d9deb7d47e9bc94209aacdc6e72834
shane-gooch <43436342+shane-gooch@users.noreply.github.com>
2019-07-14 19:29:36 -0700

    added login functionality

--- src/App.js
+++ src/App.js
@@ -1,60 +1,11 @@
 import React from "react";
-import "./App.scss";
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import { connect } from "react-redux";
-import { addBill, getBills, updateTrip } from "./actions";
+import Login from "./components/Login/Login";
 
-import Trip from "./view/HomeContainer/Trip";
-import TripForm from "./view/HomeContainer/TripForm";
-import Login from "./view/Login";
-import SignUp from "./view/SignUp";
-import HomeContainer from "./view/HomeContainer/HomeContainer";
-import PrivateRoute from "./view/PrivateRoute";
-import NavbarIn from "./NavbarIn";
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          {/* <Route path='/users' component={NavbarIn} /> */}
-          <Route exact path="/" component={Login} />
-          <Route path="/signup" component={SignUp} />
-          <Route
-            exact
-            path="/users/:tripid"
-            render={props => <Trip {...props} />}
-          />
-          <Route
-            path="/users/:tripid/trip"
-            render={props => (
-              <TripForm
-                {...props}
-                updateTrip={this.props.updateTrip}
-                getBills={this.props.getBills}
-              />
-            )}
-          />
-          <PrivateRoute exact path="/users" component={HomeContainer} />
-        </Router>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 }
-
-const maptStateToProps = state => {
-  console.log(state);
-  return {
-    error: state.error,
-    fetchingTrips: state.fetchingTrips,
-    trips: state.trips,
-    bills: state.bills,
-    fetchingBills: false
-  };
-};
-
-export default connect(
-  maptStateToProps,
-  { addBill, updateTrip, getBills }
-)(App);
--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-
-import rootReducer from './reducers';
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-ReactDOM.render(
-
-    <Provider store={store}>
-       <App />
-    </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
